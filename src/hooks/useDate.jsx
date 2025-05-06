407ce41de66e6cefe5043af336fafe215f62bf0e
CarloDettori <dettoric07@gmail.com>
2025-05-06 15:28:54 +0200

    better useEffect menagement

--- src/hooks/useDate.jsx
+++ src/hooks/useDate.jsx
@@ -4,9 +4,10 @@ export default function useDate() {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        setInterval(() => {
+        const interval = setInterval(() => {
             setDate(new Date());
         }, 1000);
+        return () => clearInterval(interval);
     }, []);
 
     return date;
